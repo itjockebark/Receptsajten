e34886187827aca33bc0e8f508551e10a1429d31
Jack Hoffsten <jack.hoffsten@hotmail.se>
2022-10-10 11:40:01 +0200

    cleaned

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -3,36 +3,34 @@
 </template>
 
 <script>
-import Recipes from '../components/Recipes.vue'
+  import Recipes from '../components/Recipes.vue';
 
-const baseUrl = 'https://jau21-grupp3-z5h3yg8ogjvb.sprinto.se';
+  const baseUrl = 'https://jau21-grupp3-z5h3yg8ogjvb.sprinto.se';
 
-export default {
-  components: {
-    Recipes
-  },
-  data() {
-    return {
-      recipes: []
-    }
-  },
-  created() {
-    this.$watch(
-      () => this.$route.params,
-      () => {
-        this.fetchData()
-      },
-      { immediate: true }
-    )
-  },
-  methods: {
-    async fetchData() {
-      console.log(this.$route.params.name)
-      const response = await fetch(`${baseUrl}/categories/${this.$route.params.name}/recipes`)
-      const data = await response.json();
-      this.recipes = data;
-
+  export default {
+    components: {
+      Recipes
     },
+    data() {
+      return {
+        recipes: []
+      }
+    },
+    created() {
+      this.$watch(
+        () => this.$route.params,
+        () => {
+          this.fetchData();
+        },
+        { immediate: true }
+      )
+    },
+    methods: {
+      async fetchData() {
+        const response = await fetch(`${baseUrl}/categories/${this.$route.params.name}/recipes`);
+        const data = await response.json();
+        this.recipes = data;
+      }
+    }
   }
-}
 </script>
